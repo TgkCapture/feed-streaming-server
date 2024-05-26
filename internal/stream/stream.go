fc98f8059078dabe9d8a26556c8db1a125c18074
TgkCapture <tawongakanyenda5@gmail.com>
2024-05-26 21:54:50 +0200

    feat: stream recent uploaded video

--- internal/stream/stream.go
+++ internal/stream/stream.go
@@ -2,32 +2,88 @@ package stream
 
 import (
 	"feed-streaming-server/internal/utils"
+	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 )
 
 func HandleStream(w http.ResponseWriter, r *http.Request) {
+    uploadDir := "./internal/utils/uploaded_videos"
+    if err := ensureDir(uploadDir); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
     if r.Method == http.MethodPost {
         // Handle incoming stream
-        file, _, err := r.FormFile("video")
+        file, header, err := r.FormFile("video")
         if err != nil {
-            utils.ErrorLogger.Printf("Error receiving stream: %v", err)
             http.Error(w, err.Error(), http.StatusBadRequest)
             return
         }
         defer file.Close()
 
-        // Simulate saving or broadcasting the video stream
-        utils.InfoLogger.Println("Receiving stream")
-        io.Copy(io.Discard, file)
+        // Generate a unique filename
+        filename := utils.GenerateUniqueFilename(header.Filename)
+
+        f, err := os.Create(filepath.Join(uploadDir, filename))
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        defer f.Close()
+
+        _, err = io.Copy(f, file)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+        utils.InfoLogger.Printf("Uploaded video saved as: %s", filename)
+
         w.WriteHeader(http.StatusOK)
-        w.Write([]byte("Stream received"))
+        w.Write([]byte("Uploaded video saved successfully"))
     } else if r.Method == http.MethodGet {
-        // Stream to the viewer
-        utils.InfoLogger.Println("Streaming video to viewer")
-        http.ServeFile(w, r, "./internal/utils/nature.mp4")
+        // Stream to the viewer the lastest file
+        latestFile, err := getLatestFile(uploadDir)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        http.ServeFile(w, r, filepath.Join(uploadDir, latestFile))
     } else {
-        utils.ErrorLogger.Println("Invalid request method")
         http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
     }
+}
+
+func ensureDir(dirName string) error {
+    err := os.MkdirAll(dirName, os.ModePerm)
+    if err != nil {
+        return fmt.Errorf("failed to create directory: %w", err)
+    }
+    return nil
+}
+
+func getLatestFile(dirPath string) (string, error) {
+    var latestModTime time.Time
+    var latestFile string
+
+    err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+        if !info.IsDir() && info.ModTime().After(latestModTime) {
+            latestModTime = info.ModTime()
+            latestFile = info.Name()
+        }
+        return nil
+    })
+    if err != nil {
+        return "", err
+    }
+
+    return latestFile, nil
 }
