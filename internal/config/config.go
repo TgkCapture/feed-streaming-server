31b947607b86e91945f49f04d8adf5d5e0fe3a79
TgkCapture <tawongakanyenda5@gmail.com>
2024-06-18 14:39:16 +0200

    feat: Update the Config struct and LoadConfig function

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,15 +1,18 @@
 package config
 
 import (
-	"github.com/TgkCapture/feed-streaming-server/internal/utils"
-	"os"
-	"github.com/joho/godotenv"
+    "github.com/TgkCapture/feed-streaming-server/internal/utils"
+    "os"
+    "github.com/joho/godotenv"
 )
 
 type Config struct {
     SenderPort   string
     ReceiverPort string
     StreamKey    string
+    DBUser       string
+    DBPassword   string
+    DBName       string
 }
 
 func LoadConfig() *Config {
@@ -21,6 +24,9 @@ func LoadConfig() *Config {
     return &Config{
         SenderPort:   os.Getenv("SENDER_PORT"),
         ReceiverPort: os.Getenv("RECEIVER_PORT"),
-        StreamKey:  os.Getenv("STREAM_KEY"),
+        StreamKey:    os.Getenv("STREAM_KEY"),
+        DBUser:       os.Getenv("DB_USER"),
+        DBPassword:   os.Getenv("DB_PASSWORD"),
+        DBName:       os.Getenv("DB_NAME"),
     }
 }
