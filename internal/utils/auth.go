d7644c63083740fdb08b8a2e586523da584e940e
TgkCapture <tawongakanyenda5@gmail.com>
2024-06-19 21:31:15 +0200

    feat; added custom context key

--- internal/utils/auth.go
+++ internal/utils/auth.go
@@ -1,12 +1,17 @@
 package utils
 
 import (
-    "net/http"
+	"context"
+	"net/http"
 	"os"
-    "time"
+	"time"
 
-    "github.com/dgrijalva/jwt-go"
+	"github.com/dgrijalva/jwt-go"
 )
+
+type contextKey string
+
+const usernameKey contextKey = "username"
 
 var jwtKey = []byte(os.Getenv("JWT_SECRET_KEY"))
 
@@ -34,14 +39,23 @@ func GenerateJWT(username string) (string, error) {
 func Authenticate(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         tokenStr := r.Header.Get("Authorization")
+        if tokenStr == "" {
+            http.Error(w, "Authorization header missing", http.StatusUnauthorized)
+            return
+        }
+
         claims := &Claims{}
         token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
             return jwtKey, nil
         })
         if err != nil || !token.Valid {
-            http.Error(w, "Unauthorized", http.StatusUnauthorized)
+            http.Error(w, "Invalid or expired token", http.StatusUnauthorized)
             return
         }
+
+        ctx := context.WithValue(r.Context(), usernameKey, claims.Username)
+        r = r.WithContext(ctx)
+
         next.ServeHTTP(w, r)
     })
 }
